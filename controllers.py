456b01abc145945a6d3593ec73b91efc31e0b3e8
Mamisoa at home <mamisoa@gmail.com>
2021-02-15 17:52:43 +0100

    Adjust API with json

--- controllers.py
+++ controllers.py
@@ -86,9 +86,10 @@ from pydal.restapi import RestAPI, Policy
 policy = Policy()
 policy.set('*','GET', authorize=True, allowed_patterns=['*'])
 policy.set('*','POST', authorize=True)
+policy.set('*','PUT', authorize=True)
 
 @action('api/<tablename>/', method=['GET','POST'])
 @action('api/<tablename>/<rec_id>', method=['GET','PUT','DELETE'])
 def api(tablename, rec_id=None):
-    return RestAPI(db,policy)(request.method,tablename,rec_id,request.GET,request.POST)
-
+    return RestAPI(db,policy)(request.method,tablename,rec_id,request.GET,request.json)
+    # return RestAPI(db,policy)(request.method,tablename,rec_id,request.GET,request.POST) 
