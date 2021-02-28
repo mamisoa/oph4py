541cf0b2f10ebf7724625cbed77baff71273a7ed
Mamisoa at home <mamisoa@gmail.com>
2021-02-28 14:55:48 +0100

    Working search field

--- static/js/test.js
+++ static/js/test.js
@@ -1,4 +1,39 @@
-// client side: return array
-function responseHandler(res) {
-    return res.items;
+function responseHandler(res) { // used if data-response-handler="responseHandler"
+    list = res.items;
+    nbrows = Object.keys(list).length;
+    display = [];
+    $.each(list, function (i) {
+        display.push({
+            'id': list[i].id,
+            'uid': list[i].uid,
+            'first_name': list[i].first_name,
+            'last_name': list[i].last_name,
+            'dob': list[i]['dob'],
+            'gender': list[i]['gender.sex'],
+        });
+    });
+    return display;
+}
+
+// use:
+// data-ajax="ajaxRequest"
+// data-side-pagination="client"
+// to be able to use search in javascript
+// or just use: data-query-params=""
+function ajaxRequest(params) {
+    $.ajax({
+        type: "GET",
+        url: API_USER_LIST,
+        dataType: "json",
+        success: function (data) { 
+			console.log(data);
+			params.success({ 
+			"rows": data.items, 
+			"total": data.items.length 
+		    }) 
+        }, 
+        error: function (er) {
+            params.error(er);
+        }
+    });
 }
