138a52ffc101dafc4e79038d515496ae02e5a512
mamisoa <mamisoa@gmail.com>
2021-03-11 22:37:38 +0100

    Add row

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 # Management controllers
 
 from py4web import action, request, abort, redirect, URL, Field, response # add response to throw http error 400
-from yatl.helpers import A, XML
+from yatl.helpers import A, XML, OPTION, CAT
 from .common import db, session, T, cache, auth, logger, authenticated, unauthenticated, flash
 from pydal.validators import CRYPT # to encrypt passwords
 
@@ -52,46 +52,22 @@ def check_duplicate(form):
             form.errors['username'] = T('Username already taken')
             form.errors['email'] = ""
 
-@action('user', method=['POST','GET'])
-@action('user/<rec_id>', method=['POST','GET','PUT','DELETE'])
-@action.uses('manage/user.html', session, auth.user, db, flash)
-def user(rec_id=None):
+@action('user')
+@action('user/<rec_id>')
+@action.uses('manage/user.html', session, auth, db, flash)
+def user(rec_id="1"):
     user = auth.get_user()
-    form = Form([
-        Field('first_name'),
-        Field('last_name'),
-        Field('email'),
-        Field('username'),
-        Field('password'),
-        Field('membership'),
-        Field('gender')],
-        validation=check_duplicate
-        )
+    row = db(db.auth_user.id == rec_id).select().first()
+    username = row.username
+    membership = row.membership
     roleOptions=""
     for role in db(db.membership.id>0).select(db.membership.ALL):
         if role.membership == "Patient": # make "Patient" as default option
-            roleOptions += "<option selected value='"
+            roleOptions = CAT(roleOptions, OPTION(role.membership + " (level " + str(role.hierarchy) + ")",_selected="selected",_value=str(role.id)))
         else:
-            roleOptions += "<option value='"
-        roleOptions += str(role.id) + "'>" + role.membership + " (level " + str(role.hierarchy) + ")</option>"
-        roleOptions = XML(roleOptions)
+            roleOptions = CAT(roleOptions, OPTION(role.membership + " (level " + str(role.hierarchy) + ")",_value=str(role.id)))
+    roleOptions = XML(roleOptions)
     genderOptions = dropdownSelect(db.gender,db.gender.fields[1],1) 
-    form_key=form.formkey # identify the form, formname="None"
-    if form.accepted:
-        db.auth_user.insert(first_name=form.vars['first_name'].capitalize(),
-            last_name=form.vars['last_name'].capitalize(),
-            username=form.vars['username'],
-            email=form.vars['email'],
-            password=str(CRYPT()(form.vars['password'])[0]),
-            membership=form.vars['membership'],
-            gender=form.vars['gender'],
-            )
-        db.commit()
-        flash.set("User "+form.vars['username']+" added", sanitize=True)
-        redirect(URL('index'))
-    elif form.errors:
-        flash.set(form.errors['email']+' '+form.errors['username'])
-        redirect(URL('index'))
     return locals()
 
 @action('users_grid', method=['POST','GET'])
