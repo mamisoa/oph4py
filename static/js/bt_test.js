0ebab375d280775d946175b86a7fd249796cb8ff
Mamisoa at home <mamisoa@gmail.com>
2021-03-16 21:59:11 +0100

    Order js files for boostrap-table to work

---
+++ static/js/bt_test.js
@@ -0,0 +1,110 @@
+// set parameters for ajax request from bootstrap-table
+var s="";
+var toggle=""
+function queryParams(params) {
+    search = params.search.split(",");
+    if (search == [""]) {
+        s =""
+    } else {
+        if (search[0]!= undefined) {
+            s = "last_name.startswith=" + capitalize(search[0]);
+        } else {
+            s = "";
+        }
+        if (search[1]!= undefined) {
+            s += "&first_name.startswith=" + capitalize(search[1]);
+        } else {
+            s +="";
+        }
+        if (search[2] != undefined) {
+            s += "&gender.sex.startswith=" + capitalize(search[2]);
+        } else {
+            s += "";
+        }
+    }
+    if (params.sort != undefined) {
+        if (params.sort == "gender.sex") {
+            params.sort = "gender";
+        }
+        if (toggle=="") {
+            s += "&@order="+params.sort;
+            toggle="~";
+        } else {
+            s += "&@order=~"+params.sort;
+            toggle="";
+        }
+    }
+    if (params.offset != "0") {
+        console.log(params.offset);
+        s += "&@offset="+params.offset;
+    }
+
+    if (params.limit != "0") {
+        console.log(params.offset);
+        s += "&@limit="+params.limit;
+    }
+    console.log('s',s);
+    return decodeURI(encodeURI(s));
+}
+
+function operateFormatter(value, row, index) {
+    return [
+      '<a class="edit" href="javascript:void(0)" title="Edit user">',
+      '<i class="fas fa-edit"></i>',
+      '</a>  ',
+      '<a class="remove" href="javascript:void(0)" title="Remove">',
+      '<i class="fas fa-trash-alt"></i>',
+      '</a>'
+    ].join('')
+  };
+
+window.operateEvents = {
+    'click .like': function (e, value, row, index) {
+      alert('You click like action, row: ' + JSON.stringify(row))
+    },
+    'click .remove': function (e, value, row, index) {
+        delUser(row.id);
+    }
+  };
+
+  /// Bootstrap-table options not used here
+
+function responseHandler(res) { // used if data-response-handler="responseHandler"
+list = res.items;
+nbrows = Object.keys(list).length;
+display = [];
+$.each(list, function (i) {
+    display.push({
+        'id': list[i].id,
+        'uid': list[i].uid,
+        'first_name': list[i].first_name,
+        'last_name': list[i].last_name,
+        'dob': list[i]['dob'],
+        'gender': list[i]['gender.sex'],
+    });
+});
+return display;
+}
+
+// use:
+// data-ajax="ajaxRequest"
+// data-side-pagination="client"
+// to be able to use search in javascript
+// or just use: data-query-params=""
+function ajaxRequest(params) {
+$.ajax({
+    type: "GET",
+    url: API_USER_LIST,
+    dataType: "json",
+    success: function (data) { 
+        console.log(data);
+        params.success({ 
+        "rows": data.items, 
+        "total": data.items.length 
+        }) 
+    }, 
+    error: function (er) {
+        params.error(er);
+    }
+});
+}
