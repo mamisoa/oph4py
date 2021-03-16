5ce4d218eeb55025db797d801b397be269eb36fc
Mamisoa at home <mamisoa@gmail.com>
2021-03-16 21:59:11 +0100

    Order js files for boostrap-table to work

--- static/js/test.js
+++ static/js/test.js
@@ -1,53 +1,3 @@
-
-// set parameters for ajax request from bootstrap-table
-var s="";
-var toggle=""
-function queryParams(params) {
-    search = params.search.split(",");
-    if (search == [""]) {
-        s =""
-    } else {
-        if (search[0]!= undefined) {
-            s = "last_name.startswith=" + capitalize(search[0]);
-        } else {
-            s = "";
-        }
-        if (search[1]!= undefined) {
-            s += "&first_name.startswith=" + capitalize(search[1]);
-        } else {
-            s +="";
-        }
-        if (search[2] != undefined) {
-            s += "&gender.sex.startswith=" + capitalize(search[2]);
-        } else {
-            s += "";
-        }
-    }
-    if (params.sort != undefined) {
-        if (params.sort == "gender.sex") {
-            params.sort = "gender";
-        }
-        if (toggle=="") {
-            s += "&@order="+params.sort;
-            toggle="~";
-        } else {
-            s += "&@order=~"+params.sort;
-            toggle="";
-        }
-    }
-    if (params.offset != "0") {
-        console.log(params.offset);
-        s += "&@offset="+params.offset;
-    }
-
-    if (params.limit != "0") {
-        console.log(params.offset);
-        s += "&@limit="+params.limit;
-    }
-    console.log(s);
-    return decodeURI(encodeURI(s));
-}
-
 // catch submit userForm
 $('#userForm').submit(function(e) {
     e.preventDefault();
@@ -94,27 +44,6 @@ function crudUser(id='0',req='POST',data) {
             };
         });
 }
-
-
-function operateFormatter(value, row, index) {
-    return [
-      '<a class="edit" href="javascript:void(0)" title="Edit user">',
-      '<i class="fas fa-edit"></i>',
-      '</a>  ',
-      '<a class="remove" href="javascript:void(0)" title="Remove">',
-      '<i class="fas fa-trash-alt"></i>',
-      '</a>'
-    ].join('')
-  };
-
-window.operateEvents = {
-    'click .like': function (e, value, row, index) {
-      alert('You click like action, row: ' + JSON.stringify(row))
-    },
-    'click .remove': function (e, value, row, index) {
-        delUser(row.id);
-    }
-  };
 
 function delUser (id) {
     bootbox.confirm({
@@ -167,47 +96,4 @@ function capitalize(str) {
 // password generator
 function passGen() {
     return Math.random().toString(36)+Math.random().toString(36).toUpperCase().split('').sort(function(){return 0.5-Math.random()}).join('')
-}
-
-
-/// Bootstrap-table options not used here
-
-function responseHandler(res) { // used if data-response-handler="responseHandler"
-    list = res.items;
-    nbrows = Object.keys(list).length;
-    display = [];
-    $.each(list, function (i) {
-        display.push({
-            'id': list[i].id,
-            'uid': list[i].uid,
-            'first_name': list[i].first_name,
-            'last_name': list[i].last_name,
-            'dob': list[i]['dob'],
-            'gender': list[i]['gender.sex'],
-        });
-    });
-    return display;
-}
-
-// use:
-// data-ajax="ajaxRequest"
-// data-side-pagination="client"
-// to be able to use search in javascript
-// or just use: data-query-params=""
-function ajaxRequest(params) {
-    $.ajax({
-        type: "GET",
-        url: API_USER_LIST,
-        dataType: "json",
-        success: function (data) { 
-			console.log(data);
-			params.success({ 
-			"rows": data.items, 
-			"total": data.items.length 
-		    }) 
-        }, 
-        error: function (er) {
-            params.error(er);
-        }
-    });
 }
