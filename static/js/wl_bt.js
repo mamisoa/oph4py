5ba4d6d01c53e91ff96453c4055355f02942e494
Mamisoa at home <mamisoa@gmail.com>
2021-03-29 22:40:26 +0200

    Set modality_name <-> modality controller const

--- static/js/wl_bt.js
+++ static/js/wl_bt.js
@@ -130,11 +130,8 @@ window.operateEvents_wl = {
         }
     },
     'click .modality_ctr': function (e, value, row, index) {
-        let idModalityCtr = modalityDict[row.modality];
-        console.log(row.modality, idModalityCtr);
-        // link needs: wlId, patientId
-        // todo: left joint in python to get modality_ctr_name
-        link = HOSTURL+'/myapp/modalityCtr/tono/'+row.id
+        let controller = modalityDict[row.modality];
+        link = HOSTURL+'/myapp/modalityCtr/'+controller+'/'+row.id
         window.location.href = link;
     }
 };
