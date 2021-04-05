da2d32965d5110f4fb97d4be2619f67b23b0e0a1
Mamisoa at home <mamisoa@gmail.com>
2021-04-05 12:31:40 +0200

    Add note to km table

--- static/js/autorx_bt.js
+++ static/js/autorx_bt.js
@@ -41,6 +41,28 @@ function responseHandler(res) { // used if data-response-handler="responseHandle
                 };
 };
 
+function responseHandler_km(res) { // used if data-response-handler="responseHandler_wl"
+    let list = res.items;
+    let display = [];
+    $.each(list, function (i) {
+        display.push({
+            'id': list[i].id,
+            'id_auth_user': list[i].id_auth_user,
+            'id_worklist': list[i].id_worklist,
+            'timestamp': list[i]['timestamp'].split('T').join(' '),
+            
+            'note': list[i].note,
+            'modified_by': list[i]['mod.last_name']+' '+list[i]['mod.first_name'],
+            'modified_on': list[i]['modified_on'],
+            'created_by': list[i]['creator.last_name']+' '+list[i]['creator.first_name'],
+            'created_on': list[i]['created_on']
+        });
+    });
+    return {    rows: display, 
+                total: res.count,
+                };
+};
+
 function queryParams(params) {
     var s="";
     if (params.offset != "0") {
