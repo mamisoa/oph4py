6e9b1b736988426c745564dc05335ee4c087d100
Mamisoa at home <mamisoa@gmail.com>
2021-04-10 11:03:51 +0200

    Separate patient-bar js html

---
+++ static/js/patient-bar.js
@@ -0,0 +1,59 @@
+// get wl details
+var wlItemObj;
+
+function getWlDetails(wlId){
+    return Promise.resolve(
+        $.ajax({
+            type: 'GET',
+            dataType: 'json',
+            url: HOSTURL+"/myapp/api/worklist/"+wlId+"?@lookup=patient!:id_auth_user[id,last_name,first_name,dob,photob64],modality!:modality_dest[id,modality_name],provider!:provider[id,last_name,first_name,dob],senior!:senior[id,last_name,first_name,dob]",
+            success: function(data) {
+                if (data.status != 'error' || data.count) {
+                    displayToast('success', 'GET combo exams', 'GET'+data.items[0]['patient.first_name']+' '+data.items[0]['patient.last_name'],3000);
+                } else {
+                    displayToast('error', 'GET error', 'Cannot retrieve combo exams');
+                }
+            }, // success
+            error: function (er) {
+                console.log(er);
+            }
+        })
+    ); // promise return data
+};
+
+getWlDetails(wlId)
+    .then(function (data) {
+        // console.log(data.items[0]);
+        let html = [];
+        let dataWl = data.items[0];
+        for (item in dataWl) {
+            html.push('<p>Key: '+item+' - Value: '+dataWl[item]+'</p>');
+        }
+        $('#wldetails').append(html.join(''));
+        return data.items[0];
+    })
+    .then(function (itemObj){ // set patient ID in top bar
+        wlItemObj = Object.assign({},itemObj); // clone wltitemobj in global
+        $('input[name=id_auth_user]').val(wlItemObj['patient.id']); // set patient id in forms
+        $('input[name=id_worklist]').val(wlItemObj['id']); // set patient id in forms
+        $('#wlItemDetails .patientName').html(itemObj['patient.first_name']+' '+itemObj['patient.last_name']);
+        $('#wlItemDetails .patientDob').html(itemObj['patient.dob']+' ('+getAge(itemObj['patient.dob'])+'yo)');
+        $('#wlItemDetails .patientId').html('#'+itemObj['patient.id']);
+        $('#wlItemDetails .timeslot').html(itemObj['requested_time'].split('T').join(' '));
+        $('#wlItemDetails .modality').html(itemObj['modality.modality_name']);
+        $('#wlItemDetails .laterality').html(itemObj['laterality']);
+        $('#wlItemDetails .provider').html(itemObj['provider.first_name']+' '+itemObj['provider.last_name']);
+        $('#wlItemDetails .senior').html(itemObj['senior.first_name']+' '+itemObj['senior.last_name']);
+        $('#wlItemDetails .status').html(itemObj['status_flag']);
+        if (itemObj['status_flag'] == 'done') {
+            disableBtn(btnArr);
+        }
+        wlItemObj['warning'] != null? $('#wlItemDetails .warning').html('<i class="fas fa-exclamation-circle"></i> '+itemObj['warning']) : $('#wlItemDetails .warning').html('').removeClass('bg-danger text-wrap');
+        if (wlItemObj['patient.photob64'] == null) {
+            $('#photoDiv').addClass('visually-hidden');
+            $('#patientIdDiv').removeClass('text-end').addClass('text-center');
+        } else {
+            document.getElementById("photoId").setAttribute("src",wlItemObj['patient.photob64']);
+        };
+    });
+
