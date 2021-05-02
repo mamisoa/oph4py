ab36c115ddcbd4f1a41cfc36be36ae30c645e0f5
Mamisoa at home <mamisoa@gmail.com>
2021-05-03 00:19:43 +0200

    typos

--- static/js/glasses.js
+++ static/js/glasses.js
@@ -666,7 +666,7 @@ $('#GxRxFormModal').submit(function(e) {
             // pdf.download('rx');
             pdf.print()
             // document.getElementById('GxRxFormModal').reset();
-            $gxRx.bootstrapTable('refresh');
+            $gxrx.bootstrapTable('refresh');
             $('#GxRxModal').modal('hide');
         });
 });
