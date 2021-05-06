cddcc20073d699b05b4260c8581a4c1ae531247b
Mamisoa at home <mamisoa@gmail.com>
2021-05-07 00:28:47 +0200

    European style dob in patient bar

--- static/js/patient-bar.js
+++ static/js/patient-bar.js
@@ -3,7 +3,7 @@
 $('input[name=id_auth_user]').val(patientObj['id']); // set patient id in forms
 $('input[name=id_worklist]').val(wlObj['worklist']['id']); // set patient id in forms
 $('#wlItemDetails .patientName').html(patientObj['first_name']+' '+patientObj['last_name'].toUpperCase());
-$('#wlItemDetails .patientDob').html(patientObj['dob']+' ('+getAge(patientObj['dob'])+'yo)');
+$('#wlItemDetails .patientDob').html(patientObj['dob'].split('-').reverse().join('/')+' ('+getAge(patientObj['dob'])+'yo)');
 $('#wlItemDetails .patientId').html('#'+patientObj['id']);
 $('#wlItemDetails .timeslot').html(wlObj['worklist']['requested_time'].split('T').join(' '));
 $('#wlItemDetails .modality').html(wlObj['modality']['modality_name']);
