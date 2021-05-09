239e0fb0fe826c73162f7c17e8cf3db6b6eeb63a
Mamisoa at home <mamisoa@gmail.com>
2021-05-09 19:46:48 +0200

    Cannot refresh tables after back button

--- static/js/glasses.js
+++ static/js/glasses.js
@@ -666,7 +666,7 @@ $('#GxRxFormModal').submit(function(e) {
             // pdf.download('rx');
             pdf.print()
             // document.getElementById('GxRxFormModal').reset();
-            $gxrx.bootstrapTable('refresh');
+            $gxrx_tbl.bootstrapTable('refresh');
             $('#GxRxModal').modal('hide');
         });
 });
