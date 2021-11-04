d92ee622187f15182c039fc205646a4498915f20
Mamisoa at home <mamisoa@gmail.com>
2021-11-05 00:10:22 +0100

    fully working cv5000xml export

--- modules/topcon/cv5000xml.py
+++ modules/topcon/cv5000xml.py
@@ -14,16 +14,23 @@ def cSubElement(_parent, _tag, attrib={}, _text=None, nsmap=None, **_extra):
 
 def createCV5000xml(resDict):
     rxDict = resDict['rx']
-    mesLst = rxDict['measures'] 
+    mesLst = rxDict['measures']
+    kmDict = resDict['km']
+    meskLst = kmDict['measures'] 
+    patDict = resDict['patient']
     from lxml import etree
     from string import Template
     from pathlib import Path
     skel = Path(TOPCON_XML+'/partial_skel0txt.xml').read_text()
     nsSBJ = Path(TOPCON_XML+'/partial_nsSBJtxt.xml').read_text()
     nsSBJExamDistance = Path(TOPCON_XML+'/partial_nsSBJExamDistancetxt.xml').read_text()
+    nsKm = Path(TOPCON_XML+'/partial_nsKmtxt.xml').read_text()
+    nsKmR = Path(TOPCON_XML+'/partial_nsKmRtxt.xml').read_text()
+    nsKmL = Path(TOPCON_XML+'/partial_nsKmLtxt.xml').read_text()
+    nsCommon = Path(TOPCON_XML+'/partial_nsCommontxt.xml').read_text()
     typeno = { } # {typeNo1 : txt, typeNo2 : txt ... typeNon : txt}
-    typenotxt = []
-    partial =''
+    rxTxt =''
+    ## nsSBJ:RefractionData
     for mes in mesLst:
         if mes['TypeNo'] not in typeno:
             mes['edNo'] = 1
@@ -35,24 +42,38 @@ def createCV5000xml(resDict):
             typeno[mes['TypeNo']]['txt'] += t
     for k,v in typeno.items(): # include each ExamDistance in correponding typeNo
         t = Template(nsSBJ).safe_substitute({ "TypeNo": k, "TypeName" : typeno[k]['TypeName'], "ExamDistance" : typeno[k]['txt'] })
-        partial +=t
-    full = Template(skel).safe_substitute({ "refractionTest" : partial})
+        rxTxt +=t
+    ## nsKM:KM -> always only 0-2 measures max
+    [kmR, kmL] = ['','']
+    for mes in meskLst:
+        if mes['side'] == 'R':
+            kmR = Template(nsKmR).safe_substitute(mes)
+        if mes['side'] == 'L':
+            kmL = Template(nsKmL).safe_substitute(mes)
+    kmTxt = Template(nsKm).safe_substitute({"kmR" : kmR , "kmL" : kmL })
+    common = Template(nsCommon).safe_substitute(patDict)
+    full = Template(skel).safe_substitute({ "refractionTest" : rxTxt , "km" : kmTxt, "common": common })
     root = etree.fromstring(full)
     full = etree.tostring(root, encoding='UTF-8', xml_declaration=True ,pretty_print=True).decode()
-    # return partial
-    # return typeno
     return full
 
 @action('rest/exportCV5000xml', method=['GET'])
 def exportCV5000xml():
     import json
+    from datetime import datetime
+    now = datetime.now()
     response.headers['Content-Type'] = 'application/xml;charset=UTF-8'
     machinedict = TOPCON_DICT
     if 'machine' in request.query:
         machine = request.query.get('machine')
     else:
         machine = 'default'
+    patient = {'firstname': 'Steve', 'lastname': 'Jobs', 'patientid': '2600',
+        'gender': 'Male', 'age': '66', 'dob': '2000-01-01',
+        'date': now.strftime("%Y-%m-%d"), 'time': now.strftime("%H:%M:%S")
+        }
     # importDict = { 'machine': 'test', 'rx': { 'count':20 , 'measures': [ { 'sph': '-1.25', 'cyl' : '-0.50'} ]} }
     importDict = importCV5000(machine)
+    importDict.update({"patient" : patient})
     resDict = createCV5000xml(importDict)
     return resDict
