97835207ad8b6bd82964ca6f939ab49f95f14d06
Mamisoa <mamisoa@gmail.com>
2023-07-31 19:43:48 +0200

    Refactor eyesuite api  to extract od and os

--- modules/eyesuite/eyesuite_rest.py
+++ modules/eyesuite/eyesuite_rest.py
@@ -6,6 +6,7 @@ from ...common import session, T, cache, auth, logger, authenticated, unauthenti
 
 from ...settings import MACHINES_FOLDER, EYESUITE_FOLDER, EYESUITE_RESULTS_FOLDER
 
+
 def createWlFile(device, id='', lastname='', firstname='', dob='', sex=''):
     """
     Creates a UTF-8 encoded patient.txt for eyesuite with the provided information 
@@ -76,7 +77,7 @@ def createWlItem(machine,id='1',lastname='_',firstname='_',dob='', sex=''):
         createWLFile: create patient.txt file to eyesuite EMR directory
     
     """
-    import os,json
+    import json
     response.headers['Content-Type'] = 'application/json;charset=UTF-8'
     res = { 'result': 'error'}
     if 'lastname' in request.query:
@@ -163,34 +164,67 @@ def find_matching_xml(date: str, lastname: str, firstname: str, ID: str = '', di
     
     return matching_files,reg
 
-
-def extract_nested_elements(element, path=""):
+def normalize_data(data):
     """
-    Recursively extract nested elements and their attributes from an XML element.
-    
-    Parameters:
-    element (xml.etree.ElementTree.Element): The XML element to extract data from.
-    path (str): The current path of the element, used for recursive calls.
-    
-    Returns:
-    nested_elements (dict): A dictionary where the keys are the paths of the elements
-    and the values are the texts or attribute values.
+    Normalize the data according to the following rules:
+    - Dates are converted to the format yyyy-mm-dd
+    - Floats use a dot instead of a comma and are converted to float type
+    - CENTRAL_CORNEA_THICKNESS is converted to an integer
     """
-    nested_elements = {}
+    from datetime import datetime
+    # Normalize date
+    date_str = data['EXAMINATION']['DATE']
+    try:
+        # Try parsing the date with timestamp
+        date_obj = datetime.strptime(date_str, '%Y.%m.%d %H:%M:%S')
+    except ValueError:
+        # If it fails, try parsing the date without timestamp
+        date_obj = datetime.strptime(date_str, '%Y-%m-%d')
+    data['EXAMINATION']['DATE'] = date_obj.strftime('%Y-%m-%d')
     
-    # If the element has text, store it
-    if element.text and element.text.strip():
-        nested_elements[path] = element.text.strip()
+    # Normalize birthday
+    birthday_str = data['PATIENT']['BIRTHDAY']
+    try:
+        birthday_obj = datetime.strptime(birthday_str, '%d/%m/%Y')
+    except ValueError:
+        birthday_obj = datetime.strptime(birthday_str, '%Y-%m-%d')
+    data['PATIENT']['BIRTHDAY'] = birthday_obj.strftime('%Y-%m-%d')
     
-    # If the element has attributes, store them
-    for attr_name, attr_value in element.attrib.items():
-        nested_elements[f"{path}@{attr_name}"] = attr_value
+    # Normalize floats
+    for measurement_type in ['A-SCAN', 'KERATOMETRY', 'WHITE-WHITE', 'PUPILLOMETRY']:
+        if measurement_type in data:
+            for measurement, value in data[measurement_type].items():
+                if value is not None and ',' in value:
+                    data[measurement_type][measurement] = float(value.replace(',', '.'))
+                    
+    # Convert CENTRAL_CORNEA_THICKNESS to an integer
+    if 'A-SCAN' in data and 'CENTRAL_CORNEA_THICKNESS' in data['A-SCAN']:
+        value = data['A-SCAN']['CENTRAL_CORNEA_THICKNESS']
+        if value is not None:
+            data['A-SCAN']['CENTRAL_CORNEA_THICKNESS'] = int(float(value))
 
-    # If the element has children, recursively extract their elements
-    for child in element:
-        nested_elements.update(extract_nested_elements(child, f"{path}/{child.tag}" if path else child.tag))
+
+def extract_eye_data(eye_node, patient_info, examination_info):
+    """
+    Extracts data from an EYE node and includes patient and examination information.
+    Returns a dictionary where keys are measurement types and values are dictionaries of measurements and their values.
+    """
+    data = {"PATIENT": patient_info, "EXAMINATION": examination_info}
     
-    return nested_elements
+    # Loop through the measurement nodes (e.g., A-SCAN, KERATOMETRY, etc.)
+    for measurement_node in eye_node:
+        measurement_type = measurement_node.tag
+        data[measurement_type] = {}
+        
+        # Loop through the individual measurements and extract their values
+        for individual_measurement_node in measurement_node:
+            measurement = individual_measurement_node.tag
+            value = individual_measurement_node.text
+            data[measurement_type][measurement] = value
+    
+    # Normalize the data
+    normalize_data(data)
+    return data
 
 @action('rest/lenstar/', method=['GET']) 
 def upload_lenstar(id='',lastname='_',firstname='_'):
@@ -202,9 +236,11 @@ def upload_lenstar(id='',lastname='_',firstname='_'):
     The data is then converted to the format expected by the database and inserted into the 'examination' table.
     """
     import datetime
+    import xml.etree.ElementTree as ET, datetime
+
     path = EYESUITE_FOLDER + 'lenstar/export'
     now = datetime.date.today().strftime('%Y-%m-%d')
-    import xml.etree.ElementTree as ET, json, datetime
+
     if 'lastname' in request.query:
         lastname = request.query.get('lastname')
     if 'firstname' in request.query:
@@ -214,111 +250,28 @@ def upload_lenstar(id='',lastname='_',firstname='_'):
     filenames, reg = find_matching_xml(directory=path, date=now, lastname=lastname, firstname=firstname, ID=id)
     if len(filenames) == 0:
         return {'status': 'error', 'message': 'no xml found', 'params': [id,lastname,firstname,path,now, reg], 'filenames': filenames}
+    
     lenstar_data = {}
 
     for filename in filenames:
         # Parse the XML file
         tree = ET.parse(filename)
         root = tree.getroot()
+        try:
+            patient_node = root.find('EXAMINATION/PATIENT')
+            patient_info = {child.tag: child.text for child in patient_node}
+        except:
+            return { 'status': 'error', 'data': filename }
 
-        # Extract the data from the XML
-        data = extract_nested_elements(root)
+        examination_node = root.find('EXAMINATION')
+        examination_info = {child.tag: child.text for child in examination_node if child.tag == 'DATE'}
 
-        #check keys
-        keys = [
-            "EXAMINATION/DATE",
-            "EXAMINATION/PATIENT/NAME",
-            "EXAMINATION/PATIENT/FIRSTNAME",
-            "EXAMINATION/PATIENT/BIRTHDAY",
-            "EXAMINATION/EYE@side",
-            "EXAMINATION/EYE/A-SCAN/MODE",
-            "EXAMINATION/EYE/A-SCAN/CENTRAL_CORNEA_THICKNESS",
-            "EXAMINATION/EYE/A-SCAN/AQUEOUS_DEPTH",
-            "EXAMINATION/EYE/A-SCAN/LENSE_THICKNESS",
-            "EXAMINATION/EYE/A-SCAN/AXIAL_LENGTH",
-            "EXAMINATION/EYE/KERATOMETRY/FLAT_MERIDIAN",
-            "EXAMINATION/EYE/KERATOMETRY/STEEP_MERIDIAN",
-            "EXAMINATION/EYE/KERATOMETRY/FLAT_MERIDIAN_AXIS",
-            "EXAMINATION/EYE/WHITE-WHITE/DIAMETER",
-            "EXAMINATION/EYE/WHITE-WHITE/BARYCENTER_X",
-            "EXAMINATION/EYE/WHITE-WHITE/BARYCENTER_Y",
-            "EXAMINATION/EYE/PUPILLOMETRY/DIAMETER",
-            "EXAMINATION/EYE/PUPILLOMETRY/BARYCENTER_X",
-            "EXAMINATION/EYE/PUPILLOMETRY/BARYCENTER_Y",
-            "EXAMINATION/EYE/IMAGEAXIS",
-            "EXAMINATION/EYE/CHECK@type",
-            "EXAMINATION/EYE/CHECK@version",
-            "EXAMINATION/EYE/CHECK/QUALITY",
-            "EXAMINATION/EYE/CHECK/ERROR",
-            "EXAMINATION/EYE/CHECK/ACTIVATION",
-            "COMMENT",
-        ]
+        od_node = root.find("EXAMINATION/EYE[@side='OD']")
+        od_data = extract_eye_data(od_node, patient_info, examination_info)
 
-        default_value = ''
+        os_node = root.find("EXAMINATION/EYE[@side='OS']")
+        os_data = extract_eye_data(os_node, patient_info, examination_info)
+        
+        lenstar_data[filename]=[{'od' : od_data },{'os' : os_data }]
 
-        # Convert the data to the format expected by the database
-        lenstar_data[filename] = {
-            "date": data.get(keys[0], default_value),
-            "patient_name": data.get(keys[1], default_value),
-            "patient_firstname": data.get(keys[2], default_value),
-            "patient_birthday": data.get(keys[3], default_value),
-            "eye_side": data.get(keys[4], default_value),
-            "ascan_mode": data.get(keys[5], default_value),
-            "central_cornea_thickness": convert_to_int(data.get(keys[6], default_value)),
-            "aqueous_depth": convert_to_float(data.get(keys[7], default_value).replace(',', '.')),
-            "lense_thickness": convert_to_float(data.get(keys[8], default_value).replace(',', '.')),
-            "axial_length": convert_to_float(data.get(keys[9], default_value).replace(',', '.')),
-            "flat_meridian": convert_to_float(data.get(keys[10], default_value).replace(',', '.')),
-            "steep_meridian": convert_to_float(data.get(keys[11], default_value).replace(',', '.')),
-            "flat_meridian_axis": convert_to_int(data.get(keys[12], default_value)),
-            "white_white_diameter": convert_to_float(data.get(keys[13], default_value).replace(',', '.')),
-            "white_white_barycenter_x": convert_to_float(data.get(keys[14], default_value).replace(',', '.')),
-            "white_white_barycenter_y": convert_to_float(data.get(keys[15], default_value).replace(',', '.')),
-            "pupillometry_diameter": convert_to_float(data.get(keys[16], default_value).replace(',', '.')),
-            "pupillometry_barycenter_x": convert_to_float(data.get(keys[17], default_value).replace(',', '.')),
-            "pupillometry_barycenter_y": convert_to_float(data.get(keys[18], default_value).replace(',', '.')),
-            "imageaxis": convert_to_float(data.get(keys[19], default_value).replace(',', '.')),
-            "check_type": data.get(keys[20], default_value),
-            "check_version": data.get(keys[21], default_value),
-            "quality": convert_to_int(data.get(keys[22], default_value)),
-            "error": convert_to_int(data.get(keys[23], default_value)),
-            "activation": convert_to_int(data.get(keys[24], default_value)),
-            "comment": data.get(keys[25], default_value),
-        }
-
-
-        # Convert the data to the format expected by the database
-        # lenstar_data[filename] = {
-        #     "date": data["EXAMINATION/DATE"],
-        #     "patient_name": data["EXAMINATION/PATIENT/NAME"],
-        #     "patient_firstname": data["EXAMINATION/PATIENT/FIRSTNAME"],
-        #     "patient_birthday": data["EXAMINATION/PATIENT/BIRTHDAY"],
-        #     "eye_side": data["EXAMINATION/EYE@side"],
-        #     "ascan_mode": data["EXAMINATION/EYE/A-SCAN/MODE"],
-        #     "central_cornea_thickness": convert_to_int(data["EXAMINATION/EYE/A-SCAN/CENTRAL_CORNEA_THICKNESS"]),
-        #     "aqueous_depth": convert_to_float(data["EXAMINATION/EYE/A-SCAN/AQUEOUS_DEPTH"].replace(',', '.')),
-        #     "lense_thickness": convert_to_float(data["EXAMINATION/EYE/A-SCAN/LENSE_THICKNESS"].replace(',', '.')),
-        #     "axial_length": convert_to_float(data["EXAMINATION/EYE/A-SCAN/AXIAL_LENGTH"].replace(',', '.')),
-        #     "flat_meridian": convert_to_float(data["EXAMINATION/EYE/KERATOMETRY/FLAT_MERIDIAN"].replace(',', '.')),
-        #     "steep_meridian": convert_to_float(data["EXAMINATION/EYE/KERATOMETRY/STEEP_MERIDIAN"].replace(',', '.')),
-        #     "flat_meridian_axis": convert_to_int(data["EXAMINATION/EYE/KERATOMETRY/FLAT_MERIDIAN_AXIS"]),
-        #     "white_white_diameter": convert_to_float(data["EXAMINATION/EYE/WHITE-WHITE/DIAMETER"].replace(',', '.')),
-        #     "white_white_barycenter_x": convert_to_float(data["EXAMINATION/EYE/WHITE-WHITE/BARYCENTER_X"].replace(',', '.')),
-        #     "white_white_barycenter_y": convert_to_float(data["EXAMINATION/EYE/WHITE-WHITE/BARYCENTER_Y"].replace(',', '.')),
-        #     "pupillometry_diameter": convert_to_float(data["EXAMINATION/EYE/PUPILLOMETRY/DIAMETER"].replace(',', '.')),
-        #     "pupillometry_barycenter_x": convert_to_float(data["EXAMINATION/EYE/PUPILLOMETRY/BARYCENTER_X"].replace(',', '.')),
-        #     "pupillometry_barycenter_y": convert_to_float(data["EXAMINATION/EYE/PUPILLOMETRY/BARYCENTER_Y"].replace(',', '.')),
-        #     "imageaxis": convert_to_float(data["EXAMINATION/EYE/IMAGEAXIS"].replace(',', '.')),
-        #     "check_type": data["EXAMINATION/EYE/CHECK@type"],
-        #     "check_version": data["EXAMINATION/EYE/CHECK@version"],
-        #     "quality": convert_to_int(data["EXAMINATION/EYE/CHECK/QUALITY"]),
-        #     "error": convert_to_int(data["EXAMINATION/EYE/CHECK/ERROR"]),
-        #     "activation": convert_to_int(data["EXAMINATION/EYE/CHECK/ACTIVATION"]),
-        #     "comment": data["COMMENT"],
-        # }
-
-    # # Insert the data into the database
-    # #db.examination.insert(**db_data)
-    # # Commit the transaction
-    # # db.commit()
     return { 'status': 'success', 'data': lenstar_data }
