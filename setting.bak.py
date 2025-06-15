341d5fb2099f3331064884da969fc1428bc07754
Mamisoa <mamisoa@gmail.com>
2025-06-15 15:25:20 +0200

    add setting example

--- setting.bak.py
+++ setting.bak.py
@@ -5,18 +5,15 @@ This is an optional file that defined app level settings such as:
 - i18n settings
 This file is provided as an example:
 """
-
 NEW_INSTALLATION = False
 # ENV_STATUS='TEST' otherwise ''
-ENV_STATUS = ""
-
+ENV_STATUS=''
 import os
-from datetime import datetime
+from py4web.core import required_folder
 
 # timeoffset
 import pytz
-from py4web.core import required_folder
-
+from datetime import datetime
 
 def get_timeoffset() -> int:
     """
@@ -77,9 +74,17 @@ APP_FOLDER = os.path.dirname(__file__)
 APP_NAME = os.path.split(APP_FOLDER)[-1]
 # DB_FOLDER:    Sets the place where migration files will be created
 #               and is the store location for SQLite databases
-DB_FOLDER = required_folder(APP_FOLDER, "databases")
 
-DB_URI = "your_db_connexion"
+DB_FOLDER = required_folder(APP_FOLDER, "databases")
+DB_URI = "mysql://py4web:multigraph449xpy4webC66*@192.168.10.230:3306/py4web?set_encoding=utf8mb4"
+
+# DB_URI = "sqlite://storage.db"
+# DB_URI = "postgres://mamisoa:mamisoapy4web@localhost:5432/dbpostgres"
+# DB_URI = "mysql://py4web:mamisoapy4web@192.168.1.220/py4web?set_encoding=utf8mb4"
+# DB_URI = "mysql://py4web:multigraph449xpy4webC66*@192.168.10.235:3306/py4web?set_encoding=utf8mb4"
+# DB_URI = "mysql://py4web:multigraph449xpy4webC66*@192.168.10.229:3308/py4web?set_encoding=utf8mb"
+
+#DB_URI = "mysql://py4web:mamisoapy4web@192.168.1.229/py4web?set_encoding=utf8mb4"
 DB_POOL_SIZE = 8
 
 # DB MIGRATION
@@ -92,37 +97,39 @@ else:
     DB_MIGRATE = True
     DB_FAKE_MIGRATE = True
 
+
+# if server is moved
+#DB_MIGRATE = True
+#DB_FAKE_MIGRATE = True
+
 # location where static files are stored:
 STATIC_FOLDER = required_folder(APP_FOLDER, "static")
 
 # location where to store uploaded files:
 UPLOAD_FOLDER = required_folder(APP_FOLDER, "uploads")
 ##
-ASSETS_FOLDER = "/home/www-data/py4web/apps/" + APP_NAME + "/static/"
+ASSETS_FOLDER = '/home/www-data/py4web/apps/'+APP_NAME+'/static/'
 
 ## MACHINES
-MACHINES_FOLDER = "/oph4py/machines"
+MACHINES_FOLDER = '/oph4py/machines'
 # VISIONIX
-L80_FOLDER = "/oph4py/machines/l80/working_dir21/ClientDB"
-VX100_FOLDER = "/oph4py/machines/vx100/ClientDB"
-VX100_XML_FOLDER = "/xml"
+# L80_FOLDER = '/oph4py/machines/l80/working_dir21/ClientDB'
+L80_FOLDER = '/oph4py/machines/l80/ClientDB'
+VX100_FOLDER = '/oph4py/machines/vx100/ClientDB'
+VX100_XML_FOLDER = '/oph4py/machines/vx100/xml'
+# VX100_XML_FOLDER = '/xml'
 # CV5000
-LM_FOLDER = "/oph4py/machines/cv5000/lm"
-RM_FOLDER = "/oph4py/machines/cv5000/rm"
-TOPCON_DICT = {
-    "default": "/cv-cornea",
-    "cv-iris": "/cv-iris",
-    "cv-cornea": "/cv-cornea",
-    "cv-crist": "/cv-crist",
-}
+LM_FOLDER = '/oph4py/machines/cv5000/lm'
+RM_FOLDER = '/oph4py/machines/cv5000/rm'
+TOPCON_DICT = { 'default' : '/cv-cornea', 'cv-iris' : '/cv-iris', 'cv-cornea' : '/cv-cornea', 'cv-crist' : '/cv-crist'  }
 TOPCON_XML = required_folder(APP_FOLDER, "topcon/xml")
 
 # EYESUITE
-EYESUITE_FOLDER = "/oph4py/machines/eyesuite/"
-EYESUITE_RESULTS_FOLDER = "/oph4py/machines/eyesuite/octopus/exports/"
+EYESUITE_FOLDER = '/oph4py/machines/eyesuite/'
+EYESUITE_RESULTS_FOLDER = '/oph4py/machines/eyesuite/octopus/exports/'
 
 ## DATA
-PATIENTS_FOLDER = "/oph4py/data/patients"
+PATIENTS_FOLDER = '/oph4py/data/patients'
 
 # send email on regstration
 VERIFY_EMAIL = False
@@ -131,23 +138,23 @@ VERIFY_EMAIL = False
 REQUIRES_APPROVAL = False
 
 # ALLOWED_ACTIONS:
-# ["all"]
+# ["all"] 
 # ["login", "logout", "request_reset_password", "reset_password", "change_password", "change_email", "update_profile"]
 # if you add "login", add also "logout"
 ALLOWED_ACTIONS = ["all"]
 
 # email settings
 SMTP_SSL = False
-SMTP_SERVER = "smtp.gmail.com:587"
-SMTP_SENDER = "youremail"
-SMTP_LOGIN = "yourlogin"
+SMTP_SERVER = "smtp-relay.gmail.com:587"
+SMTP_SENDER = "secretaire@ophtalmologiste.be"
+SMTP_LOGIN = "secretaire@ophtalmologiste.be::vmpm litt lang uxiz"
 SMTP_TLS = True
-COMPANY_LOGO = "yourlogo"
+COMPANY_LOGO = "https://ophtalmologiste.be/images/logo/oeil_from_fw_h75px.png"
 
 
 # session settings
 SESSION_TYPE = "cookies"
-SESSION_SECRET_KEY = ""  # replace this with a uuid
+SESSION_SECRET_KEY = "9fdba069-106f-4c18-b758-e7745e201af4" # replace this with a uuid
 MEMCACHE_CLIENTS = ["127.0.0.1:11211"]
 REDIS_SERVER = "localhost:6379"
 
@@ -159,6 +166,7 @@ LOGGERS = [
     "error:stderr",  # error messages to stderr
     "debug:debug.log",  # debug messages to debug.log file
 ]
+
 
 # single sign on Google (will be used if provided)
 OAUTH2GOOGLE_CLIENT_ID = None
@@ -198,23 +206,30 @@ try:
 except (ImportError, ModuleNotFoundError):
     pass
 
-LOCAL_URL = "https://yourdomain.com"
-LOCAL_BEID = "https://yourdomain.com/beid/api/eid2"
+# local URL settings
+LOCAL_URL = "https://oph4py.c66.ovh"
+# LOCAL_BEID = 'https://d1.c66.ovh/beid/api/eid2'
+LOCAL_BEID = 'http://localhost:8000/beid'
+
+# LOCAL_URL = "http://192.168.1.172"
+# LOCAL_URL = "https://py4web.servers.c66"
+# LOCAL_BEID = "http://192.168.1.130/beid/api/beid"
+# LOCAL_BEID = "https://desk1.ws.c66/beid/api/beid"
 
 # defaults members
-DEFAULT_PROVIDER = ""
-DEFAULT_SENIOR = ""
+DEFAULT_PROVIDER = "Bourgeois"
+DEFAULT_SENIOR = "Andriantafika"
 
 # external db
-DB_OCTOPUS = "dbconnexion"
-DBO_FOLDER = required_folder(APP_FOLDER, "databases/octopus")
+DB_OCTOPUS= "mysql://octopusv9:multigraph449xoctopusv9@192.168.1.229/testdb"
+DBO_FOLDER= required_folder(APP_FOLDER, "databases/octopus")
 
 # PACS
 PACS = True
-AET = {"MWL": "WORKLIST", "PACS": "DCM4CHEE"}
-PACS_URL = "http://your.pacs.server:8080/dcm4chee-arc"
-REALMS_URL = "https://your.pacs.server:8843"
-KEYCLOAK_SECRET = "your_keycloak_secret"
-
-# Supplements
-SUPPLEMENT_RATIO = 1.50
+AET = {
+    'MWL' : "WORKLIST",
+    'PACS': "DCM4CHEE"
+}
+PACS_URL= "http://192.168.10.225:8080/dcm4chee-arc"
+REALMS_URL="https://192.168.10.225:8843"
+KEYCLOAK_SECRET="U2clWBRYE2RxA6udsyoOjdmlIoXFzxRp"
